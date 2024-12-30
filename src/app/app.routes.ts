0533a9608d449e86756b5fabcf2958ea77ccc3ee
Estevan Ulian <estevan.ulian@gmail.com>
2024-12-30 14:59:13 -0300

    feat: add user route with AuthGuard for account access

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,16 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
+import { UserComponent } from './pages/user/user.component';
+import { AuthGuard } from './services/authguard.service';
 
 export const routes: Routes = [
+  {
+    path: '',
+    component: UserComponent,
+    title: 'Minha conta',
+    canActivate: [AuthGuard],
+  },
   {
     path: 'login',
     component: LoginComponent,
