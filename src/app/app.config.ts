68becf28aa6fc14115a23e10b58693e13bb3d520
Estevan Ulian <estevan.ulian@gmail.com>
2024-12-30 14:58:16 -0300

    feat: enhance toastr configuration with progress bar and timeout, and add CookieService provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,12 +5,17 @@ import { provideHttpClient, withFetch } from '@angular/common/http';
 
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
+import { CookieService } from 'ngx-cookie-service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(withFetch()),
     provideAnimations(),
-    provideToastr(),
+    provideToastr({
+      progressBar: true,
+      timeOut: 3000,
+    }),
+    CookieService,
   ],
 };
